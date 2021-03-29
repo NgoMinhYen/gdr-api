97c3bef4a30e3b2c42f4f9511b344277d35ba166
duy.ngo@bacsitoancau.com <duy.ngo@bacsitoancau.com>
2021-03-29 14:25:13 +0700

    run ubuntu debug

--- src/test/java/stepdefinition/LoginSteps.java
+++ src/test/java/stepdefinition/LoginSteps.java
@@ -45,7 +45,7 @@ public class LoginSteps {
 
     @Given("I open chrome")
     public void iOpenChrome() {
-        DriverUtils.init(DriverType.CHROME);
+        DriverUtils.init(DriverType.CHROME_UBUNTU);
         DriverUtils.maximum();
     }
 
